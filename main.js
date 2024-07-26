e1b5d8debd2f52283b62eb7cb5ef53fcd2f2ce35
Victor Evangelista <140712276+victorEvangelista2@users.noreply.github.com>
2024-07-26 06:59:16 -0300

    Adição dos arquivos do projeto ChecarEmail

---
+++ main.js
@@ -0,0 +1,18 @@
+// código de validação de email
+
+// --------------------------------------------------
+function checarEmail(){
+    if(document.forms[0].email.value == "" ||
+        document.forms[0].email.value.indexOf('@') == -1 ||
+        document.forms[0].email.value.indexOf('.') == -1){
+        alert("Informe um Email válido !!!");
+        return false
+    }else {
+        document
+    }
+}
+
+// --------------------------------------------------
+
+// código de verificação do email digitado
+
